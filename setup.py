acb37b0fc42df9bf55aca2a593fe7e051604ad54
Serik <aybatov.serik@gmail.com>
2021-08-14 15:54:30 +0300

    refactor project 2

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import G2Net
+print(find_packages(where="G2Net"))
+
+setup(
+    name='G2Net',
+    version=G2Net.__version__,
+    # packages=find_packages(where="G2Net"),
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    description='Layers and models for G2Net',
+    url='https://github.com/AybatovSerik/G2Net',
+    author='Aybatov Serik',
+    author_email='aybatov.serik@gmail.com',
+)
