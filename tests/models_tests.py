10e2ed3ea3fc774334b5045b856cf3dbbcf99e08
Serik <aybatov.serik@gmail.com>
2021-08-14 15:06:16 +0300

    add __init__.py

--- tests/models_tests.py
+++ tests/models_tests.py
@@ -1,6 +1,3 @@
-import tensorflow as tf
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input
 from models.BigConvModelFor3Input import ConvModelFor3Input
 
 model = ConvModelFor3Input()
