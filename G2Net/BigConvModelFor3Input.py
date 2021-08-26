2ec21f850dd5535f53624fd6d32c231a9bb6c264
Serik <aybatov.serik@gmail.com>
2021-08-26 22:08:28 +0300

    add 512 version

--- G2Net/BigConvModelFor3Input.py
+++ G2Net/BigConvModelFor3Input.py
@@ -47,3 +47,47 @@ def ConvModelFor3Input(big_conv_num=5,
     output = Dense(1, activation='sigmoid')(flat_all)
     return Model(inputs=[input_0, input_1, input_2], outputs=output)
 
+
+def ConvModelFor3Input512(big_conv_num=3,
+                       filters_sequence=[[4,8],[8,16],[16,32]],
+                       kernel_size_sequence=[3,3]*3,
+                       pooling_factors=[4, 2, 2],
+                       activations=['selu']*3,
+                       normalization_types=['batch']*3,
+                       conv_nums=[[2,2]]*3,
+                       dropouts=[0.2]*3,
+                       dropout_head=0.2):
+    assert len(filters_sequence)==big_conv_num, f"filters for {len(filters_sequence)} blocks, but big_conv_num is {big_conv_num}"
+    assert len(kernel_size_sequence)==big_conv_num, f"kernel_size_sequence for {len(kernel_size_sequence)} blocks, but big_conv_num is {big_conv_num}"
+    assert len(pooling_factors)==big_conv_num, f"pooling_factors for {len(pooling_factors)} blocks, but big_conv_num is {big_conv_num}"
+    assert len(activations)==big_conv_num, f"activations for {len(activations)} blocks, but big_conv_num is {big_conv_num}"
+    assert len(normalization_types)==big_conv_num, f"normalization_types for {len(normalization_types)} blocks, but big_conv_num is {big_conv_num}"
+    assert len(conv_nums)==big_conv_num, f"conv_nums for {len(conv_nums)} blocks, but big_conv_num is {big_conv_num}"
+    assert len(dropouts)==big_conv_num, f"dropouts for {len(dropouts)} blocks, but big_conv_num is {big_conv_num}"
+    input_0 = Input(shape=[512, 15])
+    input_1 = Input(shape=[512, 15])
+    input_2 = Input(shape=[512, 15])
+    x_0 = input_0
+    x_1 = input_1
+    x_2 = input_2
+    x_all = Concatenate()([x_0, x_1, x_2])
+    for block_num in range(big_conv_num):
+        x_0, x_1, x_2, x_all = BigConvBlock4Input(filters_1=filters_sequence[block_num][0],
+                                                  filters_2=filters_sequence[block_num][1],
+                                                  kernel_size_1=kernel_size_sequence[block_num][0],
+                                                  kernel_size_2=kernel_size_sequence[block_num][1],
+                                                  pooling_factor=pooling_factors[block_num],
+                                                  activation=activations[block_num],
+                                                  normalization_type=normalization_types[block_num],
+                                                  conv_num_1=conv_nums[block_num][0],
+                                                  conv_num_2=conv_nums[block_num][1]
+                                                  )(x_0, x_1, x_2, x_all)
+        x_0, x_1, x_2, x_all = Dropout4Input(dropout=dropouts[block_num])(x_0, x_1, x_2, x_all)
+    x_0 = Flatten()(x_0)
+    x_1 = Flatten()(x_1)
+    x_2 = Flatten()(x_2)
+    x_all = Flatten()(x_all)
+    flat_all = Concatenate()([x_0, x_1, x_2, x_all])
+    flat_all = Dropout(dropout_head)(flat_all)
+    output = Dense(1, activation='sigmoid')(flat_all)
+    return Model(inputs=[input_0, input_1, input_2], outputs=output)
